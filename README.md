ba8321abeff7dc5bb3065ddd40a204ce1fc1ded1
tomide <ayotomiiide@gmail.com>
2021-04-07 04:30:55 +0100

    setup readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# TOMIDE SWAP
+The best motherfucking crypto swapping app you ever used in your motherfucking life on the testnet!
+
+
+# shit I used to build it.
+1. [Dapp University's motherfuckers tutorial]('https://www.youtube.com/watch?v=99pYGpTWcXM&t=2998s').
+2. React - [Download it here]
+3. Truffle -  [Download it here](https://www.trufflesuite.com/)
+4. Ganache - [Download it here](https://www.trufflesuite.com/ganache)
--- src/contracts/TomideSwap.sol
+++ src/contracts/TomideSwap.sol
@@ -14,6 +14,25 @@ contract TomideSwap {
     function buyTokens() public payable {
         // calculate the amount of tokens to buy
         uint tokenAmount = rate * msg.value;
+
+        // require that TomideSwap has enough tokens
+        require(token.balanceOf(address(this)) >= tokenAmount );
+
+        // Transfer tokens to the user
         token.transfer(msg.sender, tokenAmount);
+
+        // Emit an event
+        emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
+    }
+
+    function sellTokens(unit _amount) public payable {
+        //calculate the amount of tokens to buy
+        unit etherAmount = amount / rate;
+        
+        require(token.balanceOf(address(msg.sender)) >= tokenAmount );
+
+        msg.sender.transfer(this, tokenAmount);
+
+        
     }
 }
